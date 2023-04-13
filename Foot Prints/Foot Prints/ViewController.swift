c78e76fbfe79dd0a609408a2e10974a12b493e47
MichaelWhiting <michaelbwhiting2004@gmail.com>
2023-04-13 09:39:16 -0600

    First commit, setting up the project.

---
+++ Foot Prints/Foot Prints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foot Prints
+//
+//  Created by Michael Whiting on 4/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
