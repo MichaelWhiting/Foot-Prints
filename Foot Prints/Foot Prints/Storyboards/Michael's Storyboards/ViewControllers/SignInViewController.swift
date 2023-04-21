7726f20cd5685169332210ad8e9c0444cfe48e6e
MichaelWhiting <michaelbwhiting2004@gmail.com>
2023-04-21 08:42:27 -0600

    comming on 4/21/23

--- Foot Prints/Foot Prints/Storyboards/Michael's Storyboards/ViewControllers/SignInViewController.swift
+++ Foot Prints/Foot Prints/Storyboards/Michael's Storyboards/ViewControllers/SignInViewController.swift
@@ -6,10 +6,35 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        print("sign in button tapped")
+        guard let emailStr = emailTextField.text, let passwordStr = passwordTextField.text else { return }
+        
+        signIn(email: emailStr, password: passwordStr)
+    }
+}
+
+extension SignInViewController {
+    // MARK: Functions
+    
+    func signIn(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if error != nil {
+                print("Error with signing in, perhaps the user does not exist.")
+            } else {
+                print("User has been signed in.")
+            }
+        }
     }
 }
